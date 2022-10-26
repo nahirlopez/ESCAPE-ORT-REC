86e334bb0387a6e63f98e76c02df2cdadec26955
Maximokukiolka <101568966+Maximokukiolka@users.noreply.github.com>
2022-10-26 14:17:35 -0300

    juego 1

---
+++ Assets/scripts/Moviminet2D.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moviminet2D : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float jumpForce;
+    public int maxJump;
+
+    int hasJump;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        hasJump = maxJump;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        transform.eulerAngles = (new Vector3(0, 0, 0));
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.Translate(0, 0, movementSpeed);
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(0, 0, -movementSpeed);
+        //}
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(movementSpeed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && hasJump >= 0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            hasJump--;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            hasJump = maxJump;
+        }
+
+        //void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.name == "LosingCube")
+            {
+
+                Debug.Log("Contacto");
+                transform.position = new Vector3(-7.57f, -2.49f, 0f);
+            }
+
+        }
+
+    }
+
+}
+
---
+++ Assets/scripts/Reinicio2D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reinicio2D : MonoBehaviour
+{
+    public GameObject Sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision col)
+
+    {
+        if (col.gameObject.name == "LosingCube")
+        {
+            Debug.Log("ded");
+            Destroy(Sphere);
+        }
+    }
+}
+
+
