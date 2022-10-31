848afca2c434a6214dd541e8419baf29cbb548a7
Maxiikumbia <Maxkukiolka@gmail.com>
2022-10-30 23:50:11 -0300

    Juego2D

---
+++ Assets/scripts/PortalReinicio2D.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalReinicio2D : MonoBehaviour
+{
+    public GameObject Sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+
+    {
+        if (col.gameObject.name == "PortalScreamer")
+        {
+            Debug.Log("ded");
+            Destroy(Sphere);
+        }
+    
+      
+            if (col.gameObject.name == "PortalScreamer")
+            {
+
+                Debug.Log("Contacto");
+                transform.position = new Vector3(-7.57f, -2.49f, 0f);
+            }
+
+        }
+
+    }
+
+
