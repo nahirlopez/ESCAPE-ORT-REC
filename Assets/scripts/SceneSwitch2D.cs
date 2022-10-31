90fdba8271867287131fab54be08c89a2b9617a2
Maxiikumbia <Maxkukiolka@gmail.com>
2022-10-30 21:40:22 -0300

    Juego2D

---
+++ Assets/scripts/SceneSwitch2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch2D : MonoBehaviour
+{
+    
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "PuntoLlegada")
+        {
+            SceneManager.LoadScene("COMIENZO");
+            Debug.Log("Hace contacto");
+        }
+
+    }
+}
+
