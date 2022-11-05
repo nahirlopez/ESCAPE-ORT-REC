962b8a7bf58aa994c3096533dc313ab02d6def7d
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-11-05 15:04:21 -0300

    CAMBIO DE DIALOGOS TERMINADO!!

--- Assets/scripts/FPSTRUE.cs
+++ Assets/scripts/FPSTRUE.cs
@@ -30,15 +30,42 @@ public class FPSTRUE : MonoBehaviour
         if (inventario.win == true)
         {
             fps.SetActive(true);
+
             int i = 0;
-            while(i < ivo.GetComponent<DialogueName>().Dialogo.Length)
+            while (i < ivo.GetComponent<DialogueName>().Dialogo.Length)
             {
                 ivo.GetComponent<DialogueName>().Dialogo[i] = "";
+                i++;
             }
 
             ivo.GetComponent<DialogueName>().Dialogo[0] = "Ivo: ¿Y? ¿Cómo fue eso? ";
             ivo.GetComponent<DialogueName>().Dialogo[1] = "Ivo: Espero que no te hayas confundido de portal...";
             ivo.GetComponent<DialogueName>().Dialogo[2] = "Ivo: Si ves otra madera en la pecera, ¡Busca a Jero!";
+        }
+
+        if (inventario.madera2 == true)
+        {  int i = 0;
+            while (i < jero.GetComponent<DialogueName>().Dialogo.Length)
+            {
+                jero.GetComponent<DialogueName>().Dialogo[i] = "";
+                i++;
+            }
+            jero.GetComponent<DialogueName>().Dialogo[0] = "Jero: Oh.. se te hizo fácil. ";
+            jero.GetComponent<DialogueName>().Dialogo[1] = "Jero: Intenta agarrar la llave y ve a hablar con Ranzo";
+            
+        }
+
+        if (inventario.HasGoldenKey == true)
+        {
+            int i = 0;
+            while (i < ranzo.GetComponent<DialogueName>().Dialogo.Length)
+            {
+                ranzo.GetComponent<DialogueName>().Dialogo[i] = "";
+                i++;
+            }
+            ranzo.GetComponent<DialogueName>().Dialogo[0] = "Ranzo: No puedo creerlo, ¡Lo lograste!";
+            ranzo.GetComponent<DialogueName>().Dialogo[1] = "Ranzo: Ve a la salida de acá a mi derecha.";
+            ranzo.GetComponent<DialogueName>().Dialogo[2] = "Ranzo: ¡ESCAPEMOS!";
         }
     }
 
